0647bab6c1ffba858ba71d0857bc5360fe20da15
Juank <jpereavelasco@safareyes.es>
2025-11-26 13:56:26 +0100

    ej3

---
+++ prueba_thymelaf/src/main/java/com/prueba_thymelaf/controller/Controlador.java
@@ -0,0 +1,38 @@
+package com.prueba_thymelaf.controller;
+
+import com.prueba_thymelaf.Entidad.Producto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Controller
+public class Controlador {
+
+    @GetMapping
+    public String greeting(
+            @RequestParam(required = false,defaultValue = "JuanK") String name, Model model
+    ) {
+        model.addAttribute("name", name);
+        return "saludo";
+    }
+
+    @GetMapping("/productos")
+    public String listarProductos(Model model) {
+//        List<Producto> listaProductos = new ArrayList<Producto>();
+//               Producto p1 = new Producto("Bizcocho", 2.99, "comida");
+//               Producto p2 = new Producto("Pizza", 10.99, "comida");
+//               Producto p3 = new Producto("Tortilla", 7.99, "comida");
+//               listaProductos.add(p1);
+//               listaProductos.add(p2);
+//               listaProductos.add(p3);
+       //model.addAttribute("listaProductos", listaProductos);
+
+        return "lista";
+    }
+}
